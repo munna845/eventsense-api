8ca1eb3138680695f0756683b02d0e63a8fe2d14
munna845 <munnaahsan845@gmail.com>
2025-10-26 16:03:26 +0600

    Added core models: User, Event, Interest

--- events/models.py
+++ events/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    phone = models.CharField(max_length=15, blank=True, null=True)
+    interests = models.ManyToManyField('Interest', blank=True)
+
+    def __str__(self):
+        return self.username
+
+
+class Interest(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Event(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    location = models.CharField(max_length=100)
+    date = models.DateTimeField()
+    interests = models.ManyToManyField(Interest, related_name='events')
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='events')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
