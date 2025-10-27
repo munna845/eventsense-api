6d69158b10a1dab3198367bdaff0a3527c91153c
munna845 <munnaahsan845@gmail.com>
2025-10-27 19:45:43 +0600

    model register

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import User, Event, Interest
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['id', 'username', 'email', 'phone', 'get_interests']
+
+    def get_interests(self, obj):
+        return ", ".join([i.name for i in obj.interests.all()])
+    get_interests.short_description = 'Interests'
+
+
+@admin.register(Interest)
+class InterestAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'description', 'location', 'date', 'get_interests', 'created_by', 'created_at']
+
+    def get_interests(self, obj):
+        return ", ".join([i.name for i in obj.interests.all()])
+    get_interests.short_description = 'Interests'
