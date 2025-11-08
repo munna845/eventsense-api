05ab9fc3b0241fa856420490c7550b2c971e1167
munna845 <munnaahsan845@gmail.com>
2025-11-08 14:18:52 +0600

    addd recommended function

--- events/views.py
+++ events/views.py
@@ -2,6 +2,8 @@ from rest_framework import viewsets, permissions
 from django.contrib.auth import get_user_model
 from .models import Event, Interest
 from .serializers import user_serializer, Event_Serializer,Interest_serializer
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 User = get_user_model()
 
@@ -29,3 +31,38 @@ class Event_ViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(created_by=self.request.user)
+
+
+    #recomention
+    @action(detail=False, methods =['get'], url_path='recommended')
+    def recommended(self,request):
+      user = request.user
+
+      if not user.is_authenticated:
+        events =Event.objects.all().order_by('created_at')[:10]
+        serializer = self.get_serializer(events, many=True)
+        return Response(serializer.data)
+      if not user.is_authenticated:
+        events =Event.objects.all().order_by('created_at')[:10]
+        serializer = self.get_serializer(events, many=True)
+        return Response(serializer.data)
+      user_interests = user.interests.all()
+      events = Event.objects.all().filter(interests__in=user_interests).distinct()
+
+
+      if events.count() < 5 :
+        others = Event.objects.exclude(insterests__in =user_interests)
+        events = (events | others).distinct()
+
+      serializer= self.get_serializer(events,many=True)
+      return Response(serializer.data)
+    
+    
+
+    
+
+
+
+
+
+
